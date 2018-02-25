6d4ab61661302ee298658027001e185a075e31d7
Akira Laine <akira_laine@outlook.com>
2018-02-25 12:30:28 +0900

    initial commit & navbar

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <navbar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'trello-clone',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Helvetica Neue";
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div :class="$style.component">
+    <div :class="$style.title">Trello Clone</div>
+
+    <div :class="$style['auth-buttons']">
+      <a :class="$style.button">Log In</a>
+      <a :class="[$style.button, $style.primary]">Sign Up</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style module>
+.component {
+  width: 100%;
+  height: 80px;
+  background-color: #0079bf;
+  display: flex;
+  align-items: center;
+  padding: 1em .75em;
+  box-sizing: border-box;
+}
+
+.title {
+  font-weight: 600;
+  color: #fff;
+  font-size: 1.7em;
+}
+
+.auth-buttons {
+  margin-left: auto;
+}
+
+.button {
+  padding: .7em 1.3em;
+  color: #fff;
+  background: #0466a3;
+  font-weight: 400;
+  border-radius: .4em;
+  cursor: pointer;
+  box-shadow: 0 2px 0 rgba(0,0,0,0.3);
+  display: inline-block;
+}
+
+.button.primary {
+  background: linear-gradient(to bottom, #61bd4f 0, #5aac44 100%);
+  font-weight: 600;
+}
+
+.button:hover {
+  background-color: rgba(0,0,0,0.25);
+  box-shadow: 0 2px 0 rgba(0,0,0,0.4)
+}
+
+.button.primary:hover {
+  background: linear-gradient(to bottom, #5aac44 0, #519839 100%)
+}
+</style>
