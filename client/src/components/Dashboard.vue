648c12f6dcd34c3d25d4a0b56444ec328a938b84
Akira Laine <akira_laine@outlook.com>
2018-02-27 19:07:23 +0900

    start work on dashboard (navbar)

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    Dashboard
+    <navbar />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Dashboard/Navbar'
+
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  components: {
+    Navbar
+  }
 }
 </script>
 
---
+++ client/src/components/Dashboard/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="$style.component">
+    <div :class="$style.title">Trello Clone</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
+
+<style module>
+.component {
+  width: 100%;
+  padding: 5px 8px;
+  box-shadow: 0 1px 1px rgba(0,0,0,.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #026aa7;
+  color: white;
+  height: 40px;
+}
+
+.title {
+  cursor: pointer;
+  opacity: 0.5;
+}
+
+.title:hover {
+  opacity: 1;
+}
+</style>
